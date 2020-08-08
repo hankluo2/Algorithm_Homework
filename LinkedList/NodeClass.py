6c7c21c6b7c5f9cbd04f0f912c7400637dcbc257
hankluo2 <lh313hk@163.com>
2020-08-08 19:20:55 +0800

    Done with class Node

--- LinkedList/NodeClass.py
+++ LinkedList/NodeClass.py
@@ -7,5 +7,18 @@
 
 class Node:
     def __init__(self, data, next_node = None):
+        self._items = None # reference to itself
+        self.size = 0
         self.data = data
         self.next = next_node
+
+    def add(self, data):
+        """ Set default that the current node is head"""
+        self._items = Node(data, self._items)
+        self.size += 1
+        
+    def __iter__(self):
+        cursor = self._items
+        while not cursor is None:
+            yield cursor.data
+            cursor = cursor.next
