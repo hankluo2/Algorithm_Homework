eb35beb275f2533656f038585273382ca8a344b6
hankluo2 <lh313hk@163.com>
2020-08-08 18:59:09 +0800

    git add first_alg.py

---
+++ first_alg.py
@@ -0,0 +1,13 @@
+""" 
+@File First testing file
+@author Hankluo2
+@Date 2020-8-8
+@desc
+"""
+
+def Hello(self):
+    print('This is the first hello-world test.')
+
+
+if __name__ == '__main__':
+    Hello(None)
