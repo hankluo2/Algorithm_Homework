f42c76270a6cc032c1716ab9e89c7f3a67528aaf
hankluo2 <lh313hk@163.com>
2020-08-09 14:21:02 +0800

    Homework done 1-3

--- LinkedList/NodeClass.py
+++ LinkedList/NodeClass.py
@@ -6,11 +6,18 @@
 """
 
 class Node:
-    def __init__(self, data, next_node = None):
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+
+# method on Node
+class LinkedBag:
+    def __init__(self, dataList = None):
         self._items = None # reference to itself
         self.size = 0
-        self.data = data
-        self.next = next_node
+        for i in range(len(dataList) - 1, -1, -1): # insert from head
+            self.add(dataList[i])
 
     def add(self, data):
         """ Set default that the current node is head"""
@@ -19,6 +26,6 @@ class Node:
         
     def __iter__(self):
         cursor = self._items
-        while not cursor is None:
+        while cursor != None:
             yield cursor.data
             cursor = cursor.next
---
+++ LinkedList/homework.py
@@ -0,0 +1,52 @@
+"""
+@file homework.py
+@author hankluo2
+@date 2020-8-9
+@desc homework to linked list
+"""
+import NodeClass
+
+def length(head: NodeClass.Node) -> int:
+    cursor = head
+    count = 0
+    while cursor != None:
+        count += 1
+        cursor = cursor.next
+    return count
+
+
+def deleteKth(head: NodeClass.Node, k: int):
+    if k > length(head):
+        print('error: k out of range')
+        return None
+    cursor = NodeClass.Node('', head)
+    i = 0
+    while cursor != None:
+        if i + 1 == k:
+            break
+        cursor = cursor.next
+        i += 1
+    probe = cursor.next
+    cursor.next = probe.next
+    del probe
+
+
+def visitList(head: NodeClass.Node):
+    cursor = head
+    while cursor != None:
+        yield cursor.data
+        cursor = cursor.next
+
+
+if __name__ == '__main__':
+    dataList = list(input().split())
+    k = int(input())
+    head = NodeClass.LinkedBag(dataList)
+    head = head._items
+    # print(type(head))
+    print(length(head))
+    deleteKth(head, k)
+    iter = visitList(head)
+    for i in iter:
+        print(i, end=' ')
+    print()
